9c578b1207234ad472321ffbc52b46e9a94f5766
laananihamza <loenwi03@gmail.com>
2023-10-10 18:36:02 +0100

    start creating home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import './app.css'
+import HomeSection from './components/homeSection';
 function App() {
   const [section, setSection] = useState('home');
   window.addEventListener('scroll', () => {
@@ -18,6 +19,7 @@ function App() {
   return (
     <>
       <Header activeSection={section} />
+      <HomeSection />
       <div className="min-h-[100vh] w-96"></div>
       <div id='about'>Aboutttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt</div>
       <div className="min-h-[200vh] w-96"></div>
--- src/components/homeSection.jsx
+++ src/components/homeSection.jsx
@@ -1,8 +1,29 @@
+import Typewriter from "typewriter-effect";
 
 function HomeSection() {
     return (
-        <div id='home' className="w-full">
-            
+        <div id='home' className="container mx-auto px-3 md:px-5 py-10 w-full flex items-center gap-5">
+            <div className="leftside w-1/2 ">
+                <p className="welcome-text uppercase font-medium">Welcome to my portfolio</p>
+                <div className="name-text text-2xl lg:text-3xl xl:text-5xl font-semibold my-1 !leading-[1.3]">Hi, I am <span className=" text-blueHover">Hamza Laanani</span> a <Typewriter
+ 
+ onInit={(typewriter) => {
+     typewriter
+         .typeString("Web Developer")
+         .pauseFor(1000)
+         .deleteAll()
+         .typeString("Pro Programmer")
+         .pauseFor(1000)
+         .start();
+         
+ }}
+ options={{
+    loop: true
+ }}
+/></div>
+                {/* <p className="name-text text-2xl lg:text-3xl xl:text-5xl font-semibold my-1 !leading-[1.3]">Hi, I am <span className=" text-blueHover">Hamza Laanani</span> a <span id="typewriter">Web Developer</span></p> */}
+            </div>
+            <div className="w-1/2"></div>
         </div>
     )
 }
