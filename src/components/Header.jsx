3f4d00ab34aa2060aec08812bea6cfa352d1e104
laananihamza <loenwi03@gmail.com>
2023-10-11 18:09:44 +0100

    add my picture to home section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,9 +26,17 @@ function Header({ activeSection }) {
       window.removeEventListener('scroll', onScroll);
     };
   }, []);
+
+  useEffect(() => {
+    document.addEventListener('click', (e) => {
+      if (!document.querySelector('.link-section').contains(e.target) && !document.querySelector('.bars').contains(e.target)) {
+        setOpenMenu(false)
+    }
+    })
+  }, [openMenu])
   
   return (
-    <header ref={header} className={`px-10  flex items-center justify-between duration-100 w-full ${position ? '!py-3 duration-100 fixed bg-white' : 'relative bg-transparent py-5'} top-0`}>
+    <header ref={header} className={`px-10 z-[99999] flex items-center justify-between duration-100 w-full ${position ? '!py-3 duration-100 fixed bg-white shadow-md' : 'relative bg-transparent py-5 shadow-none'} top-0`}>
       <div className="logo text-blueHover text-2xl font-black">L</div>
       <div className="links">
         <ul className="hidden md:flex gap-5 text-sm uppercase font-medium">
@@ -40,18 +48,18 @@ function Header({ activeSection }) {
           <li className={`relative before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'contact' ? 'before:w-full' : 'before:w-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a href="#contact">Contact</a></li>
         </ul>
         <div className="mobile-links block md:hidden">
-          <FontAwesomeIcon icon={faBars} onClick={() => setOpenMenu(true)} size="lg" className="cursor-pointer" />
-          <div className={`link-section p-5 min-h-screen w-[40%] fixed top-0 right-0 shadow-lg duration-300 bg-white z-50 ${openMenu ? 'right-0' : '-right-96'}`}>
+          <FontAwesomeIcon icon={faBars} onClick={() => setOpenMenu(true)} size="lg" className="bars cursor-pointer" />
+          <div className={`link-section p-5 min-h-screen w-[40%] fixed top-0  duration-300 bg-white z-50 ${openMenu === true ? 'right-0' : '-right-96 !shadow-none'}`}>
           <div className="close w-fit ml-auto">
             <FontAwesomeIcon icon={faClose} onClick={() => setOpenMenu(false)} size="lg" className="text-red-500 duration-200 hover:rotate-180 cursor-pointer" />
           </div>
           <ul className="mobile flex flex-col gap-5  text-sm uppercase font-medium ">
-          <li className={`relative before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'home' ? 'before:w-full' : 'before:w-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a href="#home">Home</a></li>
-          <li className={`relative before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'about' ? 'before:w-full' : 'before:w-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a href="#about" >About</a></li>
-          <li className={`relative before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'resume' ? 'before:w-full' : 'before:w-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a href="#resume">Resume</a></li>
-          <li className={`relative before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'skills' ? 'before:w-full' : 'before:w-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a href="#skills" >Skills</a></li>
-          <li className={`relative before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'portfolio' ? 'before:w-full' : 'before:w-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a href="#portfolio">Portfolio</a></li>
-          <li className={`relative before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'contact' ? 'before:w-full' : 'before:w-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a href="#contact">Contact</a></li>
+          <li className={`relative py-2 duration-200 hover:pl-2 before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'home' ? 'before:w-full pl-2' : 'before:w-0 pl-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a className=" inline-block w-full" href="#home">Home</a></li>
+          <li className={`relative py-2 duration-200 hover:pl-2 before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'about' ? 'before:w-full pl-2' : 'before:w-0 pl-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a className=" inline-block w-full" href="#about" >About</a></li>
+          <li className={`relative py-2 duration-200 hover:pl-2 before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'resume' ? 'before:w-full pl-2' : 'before:w-0 pl-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a className=" inline-block w-full" href="#resume">Resume</a></li>
+          <li className={`relative py-2 duration-200 hover:pl-2 before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'skills' ? 'before:w-full pl-2' : 'before:w-0 pl-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a className=" inline-block w-full" href="#skills" >Skills</a></li>
+          <li className={`relative py-2 duration-200 hover:pl-2 before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'portfolio' ? 'before:w-full pl-2' : 'before:w-0 pl-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a className=" inline-block w-full" href="#portfolio">Portfolio</a></li>
+          <li className={`relative py-2 duration-200 hover:pl-2 before:absolute before:-bottom-2 before:left-0 before:bg-gradient-to-br before:from-blueHover before:to-violetHover ${activeSection === 'contact' ? 'before:w-full pl-2' : 'before:w-0 pl-0'} before:duration-200 hover:before:w-full before:h-0.5`}><a className=" inline-block w-full" href="#contact">Contact</a></li>
         </ul>
           </div>
         </div>
--- src/components/homeSection.jsx
+++ src/components/homeSection.jsx
@@ -1,29 +1,33 @@
 import Typewriter from "typewriter-effect";
+import profile from '../assets/hamza_img.png'
 
 function HomeSection() {
     return (
-        <div id='home' className="container mx-auto px-3 md:px-5 py-10 w-full flex items-center gap-5">
-            <div className="leftside w-1/2 ">
+        <div id='home' className="container mx-auto px-3 md:px-5 py-10 w-full flex flex-col md:flex-row items-center gap-5">
+            <div className="leftside w-9/12 md:w-1/2 text-center md:text-left ">
                 <p className="welcome-text uppercase font-medium">Welcome to my portfolio</p>
-                <div className="name-text text-2xl lg:text-3xl xl:text-5xl font-semibold my-1 !leading-[1.3]">Hi, I am <span className=" text-blueHover">Hamza Laanani</span> a <Typewriter
- 
- onInit={(typewriter) => {
-     typewriter
-         .typeString("Web Developer")
-         .pauseFor(1000)
-         .deleteAll()
-         .typeString("Pro Programmer")
-         .pauseFor(1000)
-         .start();
-         
- }}
- options={{
-    loop: true
- }}
-/></div>
-                {/* <p className="name-text text-2xl lg:text-3xl xl:text-5xl font-semibold my-1 !leading-[1.3]">Hi, I am <span className=" text-blueHover">Hamza Laanani</span> a <span id="typewriter">Web Developer</span></p> */}
+                <div className="name-text text-3xl md:text-[1.6rem] lg:text-3xl xl:text-5xl font-semibold my-1 !leading-[1.3]">Hi, I am <span className=" text-blueHover">Hamza Laanani</span> a <Typewriter
+                    
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString("Web Developer")
+                            .pauseFor(1000)
+                            .deleteAll()
+                            .typeString("Pro Programmer")
+                            .pauseFor(1000)
+                            .start();
+                            
+                    }}
+                    options={{
+                        loop: true
+                    }}
+                /></div>
+                <p className="name-text text-gray-600 my-3">I am a Full Stack Developer, I have strong technical skills in front-end and back-end technologies. Additionally, I have problem-solving and communication skills, and I am committed to staying up to date with the latest industry trends and emerging technologies.</p>
             </div>
-            <div className="w-1/2"></div>
+            <div className=" relative">
+                <div className="empty-box w-full h-[75%] bg-white shadow-lg -z-10 rounded-xl absolute left-1/2 bottom-10 -translate-x-1/2"></div>
+                <img src={profile} alt="" className="max-w-full h-full z-50 rounded-xl" />
+            </div>
         </div>
     )
 }
